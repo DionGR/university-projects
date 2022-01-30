a13c8f9c7b5c3ab8b02cc818ae8cbc36ae052193
Dion Rigatos <dion.rigatos@gmail.com>
2022-01-30 14:34:48 +0200

    Delete Java/Summer 2021 directory

--- Java/Summer 2021/Group_Project_Part_1/ExpenseTypeB.java
+++
@@ -1,9 +0,0 @@
-/* ExpenseType SUBCLASS
-Holds information (unit) regarding an ExpenseTypeB.
- */
-public class ExpenseTypeB extends ExpenseType {
-
-    public ExpenseTypeB(String code, String description, double maxMonthlySeverance, double severancePercentage) {
-        super(code, description, maxMonthlySeverance, severancePercentage/100);
-    }// Returns the severancePercentage divided by 100, so as to ensure that it is of correct value when multiplied (i.e. 0.5, 0.1).
-}
