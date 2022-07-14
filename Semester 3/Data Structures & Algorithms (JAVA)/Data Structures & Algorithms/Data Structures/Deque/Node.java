0291095ce6f0994c67628c4c5d4cd69ffb23fcc9
Dion Rigatos <dion.rigatos@gmail.com>
2022-07-14 15:21:18 +0300

    Add files via upload

---
+++ Semester 3/Data Structures & Algorithms (JAVA)/Data Structures & Algorithms/Data Structures/Deque/Node.java
@@ -0,0 +1,5 @@
+class Node<Item> {
+    Item data = null;
+    Node next = null;
+    Node prev = null;
+}
---
+++ Semester 3/Data Structures & Algorithms (JAVA)/Data Structures & Algorithms/Data Structures/Symbol Table/src/Symbol_Table.java
@@ -0,0 +1,34 @@
+public class Symbol_Table<Key extends Comparable<Key>, Value>{
+    
+    
+    
+    
+    
+    public Value get(Key key)
+    {
+        if (isEmpty()) return null;
+        int i = rank(key);
+        if (i < N && keys[i].compareTo(key) == 0) return vals[i];
+        else return null;
+    }
+    
+    private int rank(Key key)
+    {
+        int lo = 0, hi = N-1;
+        while (lo <= hi)
+        {
+            int mid = lo + (hi - lo) / 2;
+            int cmp = key.compareTo(keys[mid]);
+            if (cmp < 0) hi = mid - 1;
+            else if (cmp > 0) lo = mid + 1;
+            else return mid;
+        }
+        return lo;
+    }
+    
+    
+    
+    public boolean contains(Key key){
+        return get(key) != null;
+    }
+}
